5e533bebb44582790bc7bed1fdab128c363a3a96
inforsoftware <volpemarco@live.it>
2018-04-11 21:36:46 +0200

    initial 2

---
+++ controllers/Welcome.php
@@ -0,0 +1,28 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+
+	/**
+	 * application/controllers/Welcome.php
+	 */
+	public function index()
+	{
+        
+        
+        $this->template->page_header = "IoT Inc. - Progetto sensori";
+
+        if (! $this->session->login ) {
+            $this->template->load('welcome_message');
+        } else {
+            
+            // qui mettere la pagina del dashboard
+            $this->template->load('admin/dashboard');
+            
+            
+        }
+        
+        
+	}
+
+}
